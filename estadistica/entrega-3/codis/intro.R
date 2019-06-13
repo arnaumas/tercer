a72c7207427c59b85def25e5799094868789c839
Arnau Mas <arnau.mas@outlook.com>
2019-06-13 19:50:54 +0200

    ppplots i qqplots

--- estadistica/entrega-3/codis/intro.R
+++ estadistica/entrega-3/codis/intro.R
@@ -83,6 +83,10 @@ library(GeneralizedHyperbolic)
 
 phi <- as.numeric(var(rend.CAT))
 w <- as.numeric(3/(kurtosi.dia))
+ord <- sort(as.numeric(rend.CAT))
+prob <- (1:length(rend.CAT))/length(rend.CAT)
+norm <- pnorm(ord, mean(rend.CAT), sd(rend.CAT))
+nig <- pnig(ord, mu = mean(rend.CAT), delta = sqrt(phi*w), alpha = sqrt(w/phi), beta = 0)
 
 # Comparació amb una NIG
 tikz(file = "../figs/nig.tex", width = 5, height = 2.5)
@@ -90,7 +94,9 @@ nf <- layout(mat = matrix(c(1,2), 1, 2, byrow = TRUE), widths = c(2.5,2.5))
 par(mar = c(4,4,2,0.5))
 qqnig(rend.CAT, main = "QQ-plot amb una NIG", xlab = "Quantils NIG", ylab = "Quantils mostrals", mu = mean(rend.CAT), delta = sqrt(phi*w), alpha = sqrt(w/phi), beta = 0, pch = 4, col = 'gray40', line = FALSE)
 abline(0,1, lty = 'dashed', col = 'red', lw = 3)
-ppnig(rend.CAT, main = "PP-plot amb una NIG", xlab = "$F(x)$", ylab = "$F_n(x)$", mu = mean(rend.CAT), delta = sqrt(phi*w), alpha = sqrt(w/phi), beta = 0, pch = 4, col = 'gray40', line = FALSE)
+plot(prob, nig, main = "PP-plot amb una NIG", 
+     xlab = "$F_n(x)$", ylab = "$F(x)$",
+     pch = 4, col = 'gray40')
 abline(0,1, lty = 'dashed', col = 'red', lw = 3)
 dev.off()
 
@@ -98,9 +104,14 @@ dev.off()
 tikz(file = "../figs/normal.tex", width = 5, height = 2.5)
 nf <- layout(mat = matrix(c(1,2), 1, 2, byrow = TRUE), widths = c(2.5,2.5))
 par(mar = c(4,4,2,0.5))
-qqPlot(as.numeric(rend.CAT), main = "QQ-plot amb una normal", xlab = "Quantils NIG", ylab = "Quantils mostrals", distribution = "norm", param.list = list(mean = mean(rend.CAT), sd = sd(rend.CAT)), pch = 4, points.col = 'gray40')
+qqplot(qnorm((1:length(rend.CAT)/length(rend.CAT)), mean(rend.CAT), sd(rend.CAT)), ord,
+             main = "QQ-plot amb una normal", 
+             xlab = "Quantils normals", ylab = "Quantils mostrals",
+             pch = 4, col = 'gray40')
 abline(0,1, lty = 'dashed', col = 'red', lw = 3)
-ppnig(rend.CAT, main = "PP-plot amb una NIG", xlab = "$F(x)$", ylab = "$F_n(x)$", mu = mean(rend.CAT), delta = sqrt(phi*w), alpha = sqrt(w/phi), beta = 0, pch = 4, col = 'gray40', line = FALSE)
+plot(prob, norm, main = "PP-plot amb una normal", 
+     xlab = "$F_n(x)$", ylab = "$F(x)$",
+     pch = 4, col = 'gray40')
 abline(0,1, lty = 'dashed', col = 'red', lw = 3)
 dev.off()
 
